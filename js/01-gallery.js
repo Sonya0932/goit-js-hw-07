c1e4ab9f225368abf928874ece5e92dd597e92e9
Sonya0932 <108592540+Sonya0932@users.noreply.github.com>
2022-11-11 00:55:26 +0200

    create first gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery')
+
+const imagesMarkup = createCardGallery(galleryItems)
+
+galleryEl.insertAdjacentHTML('beforeend', imagesMarkup)
+
+function createCardGallery (galleryItems) {
+    return galleryItems
+    .map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>
+        `
+    }).join('')
+}
+
+galleryEl.addEventListener('click', evt => {
+    evt.preventDefault();
+    if (evt.target.nodeName !== 'IMG') {
+		return
+    }
+    
+    const itemUrl = evt.target.getAttribute('data-source')
+    console.log(itemUrl)
+
+    const instance = basicLightbox.create(`
+    <img src=${itemUrl} width="800" height="600">
+    `)
+    instance.show()
+
+    galleryEl.addEventListener('keydown', closeImageAfterKeydown)
+
+    function closeImageAfterKeydown(ev) {
+        if (ev.key === 'Escape') {
+            instance.close()
+        }
+    }
+})
+
+
