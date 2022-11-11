b5388c6f05e9dcfe723fc4c707610f7c971f93fc
Sonya0932 <108592540+Sonya0932@users.noreply.github.com>
2022-11-11 21:20:15 +0200

    create second gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery')
+
+const imagesMarkup = createCardGallery(galleryItems)
+
+galleryEl.insertAdjacentHTML('beforeend', imagesMarkup)
+
+function createCardGallery (galleryItems) {
+    return galleryItems
+    .map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `
+    }).join('')
+}
+
+galleryEl.addEventListener('click', evt => {
+    evt.preventDefault();
+    if (evt.target.nodeName !== 'IMG') {
+		return
+    }
+    
+    var lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250 });
+})
